6ed150cd49f907e6606291009b55be2e5a21e1bd
Rodion Malkov <rodionmalkov@gmail.com>
2022-07-24 21:58:33 +0500

    Секция партнёры

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
-import "./style.scss";
+var swiper = new Swiper(".partners__carousel", {
+  slidesPerView: 5,
+  autoplay: {
+    delay: 1000,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      slideToClickedSlide: true,
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      slideToClickedSlide: true,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+      slideToClickedSlide: true,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+      slideToClickedSlide: true,
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+      slideToClickedSlide: true,
+    },
+  },
+});
